6f92aa8a9d0b762e9b3bba6c82a9c78d07fd0f6f
Eduardo Fernandes <edu.fernandes@ua.pt>
2020-12-14 15:12:39 +0000

    make Folha8/Ex04 better

--- Folha8/Ex04.java
+++ Folha8/Ex04.java
@@ -3,106 +3,90 @@ import java.util.Scanner;
 public class Ex04 {
     public static final Scanner KB = new Scanner(System.in);
     public static void main(String[] args) {
-        String operacao = " ";
-        while(operacao.charAt(0)!='='){
-            System.out.print("Operação: ");
-            operacao = KB.next();
+        System.out.print("Operação: ");
+        char operacao = KB.nextLine().charAt(0);
 
-            if(operacao.charAt(0)=='+'){
-                Complexo a = lerComplexo();
-                Complexo b = lerComplexo();
-                Complexo c = sum(a,b);
-                a.print();
-                System.out.print(" + ");
-                b.print();
-                System.out.print(" = ");
-                c.print();
-                System.out.println();
-            }else if(operacao.charAt(0)=='-'){
-                Complexo a = lerComplexo();
-                Complexo b = lerComplexo();
-                Complexo c = sub(a,b);
-                a.print();
-                System.out.print(" - ");
-                b.print();
-                System.out.print(" = ");
-                c.print();
-                System.out.println();
-            }else if(operacao.charAt(0)=='*'){
-                Complexo a = lerComplexo();
-                Complexo b = lerComplexo();
-                Complexo c = mult(a,b);
-                a.print();
-                System.out.print(" * ");
-                b.print();
-                System.out.print(" = ");
-                c.print();
-                System.out.println();
-            }else if(operacao.charAt(0)=='/'){
-                Complexo a = lerComplexo();
-                Complexo b = lerComplexo();
-                Complexo c = div(a,b);
-                a.print();
-                System.out.print(" / ");
-                b.print();
-                System.out.print(" = ");
-                c.print();
-                System.out.println();
-            }else{
-                System.out.println("Operação não permitida!");
+        while(operacao != '='){
+            if(operacao!='+'&&operacao!='-'&&operacao!='*'&&operacao!='/'){
+                System.out.print("Operação nao permitida!\n");
+                System.out.print("Operação: ");
+                operacao = KB.next().charAt(0);
+                continue;
             }
+
+            Complexo a = lerComplexo();
+            Complexo b = lerComplexo();
+            Complexo c = new Complexo();
+
+            if(operacao=='+'){
+                c = sum(a,b);
+            }else if(operacao=='-'){
+                c = sub(a,b);
+            }else if(operacao=='*'){
+                c = mult(a,b);
+            }else if(operacao=='/'){
+                c = div(a,b);
+            }
+
+            a.print();
+            System.out.print(" "+operacao+" ");
+            b.print();
+            System.out.print(" = ");
+            c.print();
+            System.out.println();
+
+            System.out.print("Operação: ");
+            operacao = KB.next().charAt(0);
+            
         }
-    }  
+    }
 
     public static Complexo lerComplexo() {
+        Complexo input = new Complexo();
         System.out.println("Introduza um número complexo: ");
         System.out.print("Parte real: ");
-        double r = KB.nextDouble();
+        input.r = KB.nextDouble();
         System.out.print("Parte imag: ");
-        double i = KB.nextDouble();
+        input.i = KB.nextDouble();
 
-        Complexo input = new Complexo();
-        input.set(r,i);
         return input;
     }
 
     public static Complexo sum(Complexo a, Complexo b) {
         Complexo c = new Complexo();
-        c.set( a.r+b.r , a.i+b.i );
+        c.r = a.r+b.r;
+        c.i = a.i+b.i;
         return c;
     }
 
     public static Complexo sub(Complexo a, Complexo b) {
         Complexo c = new Complexo();
-        c.set( a.r-b.r , a.i-b.i );
+        c.r = a.r-b.r;
+        c.i = a.i-b.i;
         return c;
     }
 
     public static Complexo mult(Complexo a, Complexo b) {
         Complexo c = new Complexo();
-        c.set( a.r*b.r-a.i*b.i , a.i*b.r+a.r*b.i );
+        c.r = a.r*b.r-a.i*b.i;
+        c.i = a.i*b.r+a.r*b.i;
         return c;
     }
 
     public static Complexo div(Complexo a, Complexo b) {
         Complexo c = new Complexo();
-        c.set( (a.r*b.r-a.i*b.i)/(Math.pow(b.r,2)+Math.pow(b.i,2)) , (a.i*b.r+a.r*b.i)/(Math.pow(b.r,2)+Math.pow(b.i,2)) );
+        c.r = (a.r*b.r-a.i*b.i)/(Math.pow(b.r,2)+Math.pow(b.i,2)); 
+        c.i = (a.i*b.r+a.r*b.i)/(Math.pow(b.r,2)+Math.pow(b.i,2));
         return c;
     }
 }
-
 
 class Complexo {
     public double r = 0;
     public double i = 0;
 
-    public void set(double _r, double _i) {
-        r = _r;
-        i = _i;
-    }
-
     public void print() {
-        char signal = i>0 ? '+' : '-';
-        System.out.printf("%3.1f%c%3.1fi", r, signal, i);
+        String signal = i>0 ? "+" : "";
+        System.out.printf("%3.1f%s%3.1fi", r, signal, i);
     }
 }
