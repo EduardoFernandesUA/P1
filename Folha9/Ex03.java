e01137a686987875cc604fb1c9ab86ffe3c8cd4d
Eduardo Fernandes <edu.fernandes@ua.pt>
2021-01-07 20:24:24 +0000

    added

--- Folha9/Ex03.java
+++ Folha9/Ex03.java
@@ -4,6 +4,18 @@ public class Ex03 {
     public static final Scanner KB = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        String frase = "isto é 1 frase";
+        System.out.println("A frase: \'"+frase+"\' tem "+countWords(frase)+" palavras");
+    }
+
+    public static int countWords(String frase) {
+        int counter = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char character = frase.charAt(i);
+            if( character==' '||character=='\n'||character=='\t' ){
+                counter++;
+            }
+        }
+        return counter+1;
     }
 }
